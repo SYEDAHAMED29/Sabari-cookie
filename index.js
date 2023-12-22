61f47fc9281ef2c3d7e723ac5e3a4e5c281bc948
SYED AHAMED <Syedbeamer@Admins-MacBook-Air.local>
2023-12-22 14:53:47 +0530

    added getcookie

--- index.js
+++ index.js
@@ -10,8 +10,19 @@ function setCookie(name, value, days) {
   document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-// Example: Set a cookie named "username" with the value "John" that expires in 7 days
-setCookie("username", "John", 7);
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
+}
+
+function setTitle() {
+  document.getElementById("title").innerHTML =
+    "This is " + getCookie("username");
+}
+
+// // Example: Set a cookie named "username" with the value "John" that expires in 7 days
+// setCookie("username", "John", 7);
 
 const btn = document.getElementById("btn");
 
